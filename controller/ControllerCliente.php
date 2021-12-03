1c618053fc37142081cd621f8265295ed47c8704
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2021-12-03 09:48:48 -0300

    Método findAll Cliente criado

---
+++ controller/ControllerCliente.php
@@ -0,0 +1,44 @@
+<?php
+
+class ControllerCliente
+{
+
+    private $_method;
+    private $_modelCliente;
+    private $_idCliente;
+
+
+    public function __construct($model)
+    {
+
+        $this->_modelCliente = $model;
+        $this->_method = $_SERVER["REQUEST_METHOD"];
+
+        $json = file_get_contents("php://input");
+
+        $dadosCliente = json_decode($json);
+
+        $this->_idCliente = $dadosCliente->idCliente ?? null;
+    }
+
+    function router()
+    {
+
+        switch ($this->_method) {
+            case 'GET':
+
+                if (isset($this->_idCliente)) {
+
+                    return $this->_modelCliente->findById();
+                }
+
+                return $this->_modelCliente->findAll();
+
+                break;
+
+            default:
+
+                break;
+        }
+    }
+}
---
+++ model/ModelCliente.php
@@ -0,0 +1,46 @@
+<?php
+
+class ModelCliente
+{
+
+    private $_conexao;
+    private $_idCliente;
+    private $_nome;
+    private $_sobrenome;
+    private $_email;
+    private $_dataNascimento;
+    private $_cpf;
+    private $_telefone;
+
+    public function __construct($conexao)
+    {
+
+        // Permite receber dados .json através da requisição
+
+        $json = file_get_contents("php://input");
+        $dadosCliente = json_decode($json);
+
+        // Recebimento dos dados do postman:
+
+        $this->_idCliente = $dadosCliente->idCliente ?? null;
+        $this->_nome = $dadosCliente->nome ?? null;
+        $this->_sobrenome = $dadosCliente->sobrenome ?? null;
+        $this->_email = $dadosCliente->email ?? null;
+        $this->_dataNascimento = $dadosCliente->dataNascimento ?? null;
+        $this->_cpf = $dadosCliente->cpf ?? null;
+        $this->_telefone = $dadosCliente->telefone ?? null;
+        $this->_conexao = $conexao;
+    }
+
+    public function findAll()
+    {
+        // Montagem do script SQL
+
+        $sql = "SELECT * FROM tblCliente";
+
+        $stm = $this->_conexao->prepare($sql);
+        $stm->execute();
+
+        return $stm->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
